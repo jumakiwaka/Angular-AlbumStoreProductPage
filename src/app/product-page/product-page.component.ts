87007815758a65de1b15eb13a887f1b54ccd6038
jumaali1449 <alijuma232@gmail.com>
2019-10-24 06:36:31 +0300

    adding routes

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product-page',
-  templateUrl: './product-page.component.html',
+   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
